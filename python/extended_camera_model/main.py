2ddaf3604bbcfd5660480503d5118e3f49df584c
Marvin <marvin@lorenz-o-matic.de>
2024-08-12 09:39:23 +0200

    init

--- python/extended_camera_model/main.py
+++ python/extended_camera_model/main.py
@@ -1,7 +1,14 @@
-# Copyright (C) 2024 twyleg, Marvin-VW
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QObject
+from PyQt5.QtGui import QImage
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtQml import QQmlApplicationEngine
+from PySide6.QtGui import QPixmap
 import numpy as np
 import cv2 as cv
+import sys
+import os
 
+# Import your utility modules
 from utils.camera_model import CameraModel
 from utils.matrix_functions import Matrix_Functions
 from utils.window import Window
@@ -12,10 +19,14 @@ from utils.clipping_space import Clipping_Space
 from utils.fps_counter import FpsCounter
 from utils.vectors import CalculateNormal
 
-class Engine:
+class Engine(QObject):
+
+    imageChanged = pyqtSignal(QImage)
 
     def __init__(self):
-
+        super().__init__()  # Initialize QObject here
+        
+        # Initialize your other attributes
         self.camera_model = CameraModel(0.00452, 0.00254, 0.004, 1280, 720, 1280/2, 720/2)
         self.window = Window()
         self.clipping_space = Clipping_Space()
@@ -25,6 +36,18 @@ class Engine:
         self.V_T_Cube = Matrix_Functions.create_homogeneous_transformation_matrix(2, 0, 1, 0, 0, 0, 0)
         self.render_list = []
         self.mesh_list = []
+
+    @pyqtSlot(np.ndarray)
+    def sendImage(self, image_array):
+        # Ensure the image array is in the correct format (e.g., RGB)
+        height, width, channel = image_array.shape
+        bytes_per_line = 3 * width  # RGB has 3 channels
+        q_image = QImage(image_array.data, width, height, bytes_per_line, QImage.Format_RGB888)
+        
+        # Emit the image to QML
+        print(q_image)
+        self.imageChanged.emit(q_image)
+
 
     def fps_setter(self):
         fps = self.fps_counter.get_fps_filtered()
@@ -37,7 +60,7 @@ class Engine:
         return dot_product
 
     def main(self):
-
+        
         # file_path = r"utils\resources\VideoShip.obj"
         # struc = Structure_Generator.load_from_obj(file_path)
         # self.mesh_list.extend(struc)
@@ -45,9 +68,7 @@ class Engine:
         cube = Cube(size=1, pos_x=0, pos_y=0, pos_z=0)
         self.mesh_list.extend(cube.mesh)
 
-
         while True:
-
             self.window.handle_movement()
             self.fps_counter.update()
             self.camera_model.reset_camera_image()
@@ -58,7 +79,6 @@ class Engine:
             visiable_triangles = []
 
             for triangle in self.mesh_list:
-
                 triangle.world_points = self.camera_model.world_transform(triangle.points, self.V_T_Cube)
                 triangle.normal, normal_start, normal_end, triangle.centroids = CalculateNormal.normal(triangle.world_points)
                 transformed_normals = self.camera_model.camera_transform([normal_start, normal_end], self.C_T_V)
@@ -67,17 +87,14 @@ class Engine:
                     self.camera_model.draw_camera_image_arrow(transformed_normals[0], transformed_normals[1])
 
                 if self.is_triangle_facing_camera(triangle.normal, triangle.centroids, camera_vector_world) < 0.0:
-
                     light_direction = (1,0,0)
                     triangle.ilm = Color.intensity(light_direction, triangle.normal)
-                    print(triangle.ilm)
                     triangle.color = Color.adjust_bgr_intensity(Color.ALICE_BLUE, triangle.ilm)
 
                     triangle.camera_points = self.camera_model.world_transform(triangle.world_points, self.C_T_V)
                     visiable_triangles.append(triangle)
 
             sorted_list = sorted(visiable_triangles, key=lambda triangle: triangle.centroids[2], reverse=True)
-
             clipped_triangles = []
             clipped_triangles.extend(self.clipping_space.cube_in_space(sorted_list))
 
@@ -89,6 +106,23 @@ class Engine:
 
             self.fps_setter()
             self.window.window_show(self.camera_model)
+            self.sendImage(self.camera_model.camera_image)
 
-engine = Engine()
-engine.main()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+
+    engine_instance = Engine()
+    engine.rootContext().setContextProperty("engineInstance", engine_instance)
+
+    qml_file_path = os.path.join(os.path.dirname(__file__), "frontend", "main.qml")
+    engine.load(qml_file_path)
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    # Call the main function directly
+    engine_instance.main()
+
+    sys.exit(app.exec())
